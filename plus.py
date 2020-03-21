ca8a94f457cd1d3596051a4f063000ed2c525a43
semahoo <62483425+semahoo@users.noreply.github.com>
2020-03-22 00:34:05 +0430

    Update plus.py

--- plus.py
+++ plus.py
@@ -9,9 +9,9 @@ from base64 import b64encode
 import datetime
 
 # insert your desired GB
-_YourGB = 1000000
+_YourGB = 2
 # insert your Device ID something like this
-_yourID = "047e57ef-49b3-462b-afc0-e32a0ebd11e5"
+_yourID = "8b05adf7-82a7-41a8-9b50-6133222a3a74"
 key=None
 def generate_string(length):
     letters = string.ascii_letters + string.digits
